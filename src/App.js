71bf98b5821b97b53023a358c7fb525e5eea868f
Igor Cortez <igor.barbosa14@gmail.com>
2020-08-19 02:32:06 -0300

    implemented the functionalities

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+  const [message, setMessage] = useState(null)
+  useEffect(()=>{
+    api.get('repositories').then(response=>{
+      setRepositories(response.data)
+    })
+  },[])
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories',{
+          "url": "https://github.com/Rocketseat/umbriel",
+           "title": `umbriel ${Date.now()}`,
+           "techs": ["Node", "Express", "TypeScript"]
+         })
+
+    const newRepository = response.data
+    setRepositories([...repositories,newRepository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try{
+      const response = await api.delete(`/repositories/${id}`,)
+      if(response.status === 204){
+        const filterResult = repositories.filter(repository => repository.id !== id)
+        setRepositories(filterResult)
+      }
+    }catch(err){
+      setMessage("Não foi possível remover o repositório neste momento.Tente mais tarde")
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {repositories && repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
+        {message && <span>{message}</span>}
     </div>
   );
 }
